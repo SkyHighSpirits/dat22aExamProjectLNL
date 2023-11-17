89d6a565d18c4f0f6da08f6f7a6d5e102a87ef18
Nicolai Ene <35382666+SkyHighSpirits@users.noreply.github.com>
2023-11-17 11:38:44 +0100

    Initial Commit

---
+++ src/main/java/com/example/dat22aexamproject/Dat22aExamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.dat22aexamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dat22aExamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dat22aExamProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dat22aexamproject/Dat22aExamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dat22aexamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dat22aExamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
